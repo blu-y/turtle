8c437c94c308cd1b43bd7bd4234443fff9cce16e
blu-y <102652794+blu-y@users.noreply.github.com>
2023-09-22 15:51:31 +0900

    Update autoset.sh

--- autoset.sh
+++ autoset.sh
@@ -14,8 +14,6 @@ sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
 sudo apt update
 sudo apt install ros-humble-desktop-full -y
-# Replace ".bash" with your shell if you're not using bash
-# Possible values are: setup.bash, setup.sh, setup.zsh
 source /opt/ros/humble/setup.bash
 sudo apt install ros-humble-turtlebot4-desktop -y
 ibus-setup-hangul
@@ -27,8 +25,6 @@ sudo rosdep init
 rosdep update
 sudo apt install ~nros-humble-rqt* -y
 sudo apt install python3-colcon-common-extensions -y
-sudo apt-get install ros-humble-gazebo-msgs ros-humble-gazebo-plugins ros-humble-gazebo-ros ros-humble-gazebo-ros2-control ros-humble-gazebo-ros-pkgs -y
-sudo apt install ros-humble-octomap-ros libgoogle-glog-dev libgflags-dev -y
 printenv | grep -i ROS
 
 eval "$(cat ~/.bashrc | tail -n +10)"
