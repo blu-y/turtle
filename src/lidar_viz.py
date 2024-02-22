3882be106d28c27b717e69b6a4fbd51d6d23b116
blu-y <102652794+blu-y@users.noreply.github.com>
2024-02-23 02:37:50 +0900

    Update lidar_viz.py

--- src/lidar_viz.py
+++ src/lidar_viz.py
@@ -1,6 +1,3 @@
-### ONGOING
-
-
 #!/usr/bin/env python3
 import rclpy
 from rclpy.node import Node
@@ -8,7 +5,7 @@ from rclpy.qos import qos_profile_sensor_data
 from sensor_msgs.msg import LaserScan
 import numpy as np
 import cv2
-
+import matplotlib.cm as cm
 
 class LiDARSubscriber(Node) :
    def __init__(self) :
@@ -16,6 +13,8 @@ class LiDARSubscriber(Node) :
      self.lidar_sub = self.create_subscription(
         LaserScan, '/scan', self.lidar_cb, qos_profile_sensor_data)
      self.length = 0
+     self.res = 40
+     self.cmap = cm.get_cmap('summer', 256)
 
    def lidar_cb(self, msg) :
      self.lidar = msg.ranges
@@ -24,20 +23,26 @@ class LiDARSubscriber(Node) :
        self.m = msg.range_max
        self.length = len(self.lidar)
        self.ind = np.arange(self.length)
+       self.size = int(self.m*2*self.res)
      self.lidar = np.array(self.lidar)
      self.lidar[self.lidar==np.inf] = 0
 
-     self.viz = np.ones((int(self.m*2*50), int(self.m*2*50)))*255
+     self.viz = np.ones((self.size, self.size, 3))*255
      # x coordinate
-     x_c = np.cos(self.lidar*self.inc*self.ind)*100+600
-     y_c = -np.sin(self.lidar*self.inc*self.ind)*100+600
+     x_c = np.cos(self.inc*self.ind)*self.lidar*self.size/self.m+self.size/2
+     y_c = -np.sin(self.inc*self.ind)*self.lidar*self.size/self.m+self.size/2
      for i in range(self.length):
-       cv2.circle(self.viz,(int(x_c[i]),int(y_c[i])),2,(0,0,0))
+       cv2.circle(self.viz,(int(x_c[i]),int(y_c[i])),3,self.cm(self.lidar[i]),-1)
      self.lidar = np.resize(self.lidar, (20, self.length))
      cv2.imshow('img', self.lidar)
      cv2.imshow('viz', self.viz)
      cv2.waitKey(1)
      #print(self.lidar)
+
+   def cm(self, value):
+     color = self.cmap(value / self.m)
+     rgb_color = tuple([int(255 * c) for c in color[:3]])
+     return rgb_color
      
 def main(args=None) :
   rclpy.init(args=args)
