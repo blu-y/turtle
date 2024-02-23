33ef448ccb8f9bb58140a19656d57a8bf4357ad3
blu-y <102652794+blu-y@users.noreply.github.com>
2024-02-23 18:00:54 +0900

    Update autoset.sh

--- autoset.sh
+++ autoset.sh
@@ -6,7 +6,7 @@ sudo apt install fonts-noto-cjk-extra gnome-user-docs-ko hunspell-ko ibus-hangul
 ibus restart
 sudo apt update && sudo apt upgrade -y
 sudo apt install software-properties-common -y
-sudo add-apt-repository universe
+sudo add-apt-repository universe -y
 sudo apt update && sudo apt install curl -y
 sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
