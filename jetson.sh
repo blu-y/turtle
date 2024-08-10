4218a0b6bf55514c249b9a7d2f8b339ff208a811
blu-y <102652794+blu-y@users.noreply.github.com>
2024-08-10 16:48:27 +0900

    Update jetson.sh

--- jetson.sh
+++ jetson.sh
@@ -45,6 +45,15 @@ sudo apt install python3-bloom python3-rosdep fakeroot debhelper dh-python -y
 sudo rosdep init
 rosdep update
 
+sudo apt-get install wget gpg -y
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" |sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
+rm -f packages.microsoft.gpg
+sudo apt install apt-transport-https -y
+sudo apt update
+sudo apt install code # or code-insiders -y
+
 echo "" >> ~/.bashrc
 echo "# colcon_cd Setting " >> ~/.bashrc
 echo "source /usr/share/colcon_cd/function/colcon_cd.sh" >> ~/.bashrc
